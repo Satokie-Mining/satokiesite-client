a146c460f84ec594c8a076b66799e07a03b2617e
Boots Stribling <97563724+BootsStribling@users.noreply.github.com>
2024-10-01 15:50:43 -0500

    stub up section 1

---
+++ check-node-and-npm-version.mjs
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+import * as semver from 'semver';
+import fs from 'fs';
+const requiredVersion = '^20';
+
+const currentVersion = process.version;
+if (!semver.satisfies(currentVersion, `${requiredVersion}`)) {
+  fs.rmSync('node_modules', { recursive: true, force: true });
+  fs.rmSync('package-lock.json');
+  console.error(
+    `Error: Node.js version ${currentVersion} is not compatible with this application. Please install Nodejs version: ${requiredVersion} to install required packages. Exiting install process...`
+  );
+  process.exit(1);
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,49 @@
 import * as React from 'react';
-import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Grid2 from '@mui/material/Grid2';
 import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
-import ProTip from './ProTip';
+import XIcon from '@mui/icons-material/X';
+import Icon from '@mui/material/Icon';
+import videoUrl from './tour.mp4'
 
-function Copyright() {
-  return (
-    <Typography
-      variant="body2"
-      align="center"
-      sx={{
-        color: 'text.secondary',
-      }}
-    >
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}.
-    </Typography>
-  );
-}
+
+const VideoItem = () => <video style={{width: '100vw'}} autoPlay loop muted><source src={videoUrl} type='video/mp4' /></video>
 
 export default function App() {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-          Material UI Create React App example in TypeScript
-        </Typography>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <Paper  sx={{position: 'relative', flexGrow: 1, overflowX: 'hidden'}}>
+      <Paper sx={{backgroundColor: 'black', width: '100vw', height: '90vh', borderRadius: '0'}}>
+        <Grid2 container spacing={0} sx={{display: 'flex', justifyContent: 'flex-end'}}>
+          <Grid2 size={6} sx={{display: 'flex', justifyContent:' flex-end', position: 'absolute', zIndex: 2, margin: '2.0rem'}}>
+            <Link sx={{margin: '1.2rem', color: 'white', textDecoration: 'none'}}><Typography variant='h5'>About</Typography></Link>
+            <Link sx={{margin: '1.2rem', color: 'white', textDecoration: 'none'}}><Typography variant='h5'>Services</Typography></Link>
+            <Link sx={{margin: '1.2rem', color: 'white', textDecoration: 'none'}}><Typography variant='h5'>Satokie Midstream</Typography></Link>
+            <Link sx={{margin: '1.2rem', color: 'white', textDecoration: 'none'}}><Typography variant='h5'>FAQ</Typography></Link>
+            <Link sx={{margin: '1.2rem', color: 'white', textDecoration: 'none'}}><Typography variant='h5'>Contact</Typography></Link>
+            <XIcon sx={{margin: '1.2rem', color: 'white'}}/>
+            <Icon></Icon>
+          </Grid2>
+        </Grid2>
+        <Grid2 container spacing={0} sx={{display: 'flex', justifyContent: 'center', alignItems:'center'}}>
+          <Grid2 size={10} sx={{border: 'red solid 1px', padding: '1.0rem', position: 'absolute'}}>
+            <Typography variant='h1' sx={{color: 'white', textAlign: 'center'}}>
+              Satokie
+            </Typography>
+            <Typography variant='h1'gutterBottom={true} sx={{color: 'white', textAlign: 'center'}}>
+              Mining
+            </Typography>
+          </Grid2>
+          <Grid2 container spacing={0}>
+            <Grid2 sx={{width: '100vw', height: '90vh', border: 'green solid 1px', overflow: 'hidden'}}>
+              <VideoItem />
+            </Grid2>
+          </Grid2>
+        </Grid2>
+      </Paper>
+      <Paper sx={{backgroundColor: 'blue', width: '100vw', height: '100vh', borderRadius: '0'}}>
+
+      </Paper>
+    </Paper>
   );
 }
