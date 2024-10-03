74b1d29331e0b3600e3d37652e6048e491c8754c
Boots Stribling <97563724+BootsStribling@users.noreply.github.com>
2024-10-03 16:00:44 -0500

    stub section 2

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import { useState } from 'react';
 import {Routes, Route} from 'react-router-dom';
 import Home from './Pages/Home';
-import Nav from './components/Nav';
+import UnderConstruction from './Pages/UnderConstruction';
 
 export default function AppRouter() {
   const [active, setActive] = useState(false);
 
   return (
     <Routes>
+          <Route path='/about' element={<UnderConstruction />}/>
+          <Route path='/services' element={<UnderConstruction />}/>
+          <Route path='/midstream' element={<UnderConstruction />}/>
+          <Route path='/faq' element={<UnderConstruction />}/>
+          <Route path='/contact' element={<UnderConstruction />}/>
           <Route path='/' element={<Home isActive={active}/>}/>
-          <Route path='*' element={<div><h1>404 Please bear with us, this page is under construction!</h1><a href='/'>Return</a></div>}/>
     </Routes>
   )
 }
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,12 @@
-import * as React from 'react';
+import React from 'react';
 import Paper from '@mui/material/Paper';
-import Grid2 from '@mui/material/Grid2';
 import videoUrl from '../assets/tour.mp4';
 import VideoItem from '../components/VideoItem';
 import Nav from '../components/Nav';
-
-
+import curvySection from '../assets/curvy-section.svg';
+import {Box, Grid2, Typography} from '@mui/material';
+import rackImg from '../assets/miner-rack-sect-2.jpg';
+import siteImg from '../assets/site-sect2.webp';
 
 export default function Home(props): React.JSX.Element {
 
@@ -27,8 +28,32 @@ export default function Home(props): React.JSX.Element {
           </Grid2>
         </Grid2>
       </Paper>
-      <Paper id='home-2' sx={{backgroundColor: 'blue', width: '100vw', height: '100vh', borderRadius: '0'}}>
-
+      <Paper id='home-2' elevation={0} sx={{width: '100vw', height: '90vh', borderRadius: '0', position: 'relative', overflow: 'hidden'}}>
+        <Paper elevation={0} sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%', borderRadius:'0', position: 'absolute'}}>
+          <img style={{zIndex: 2, position: 'absolute', width: 'calc(100vh + 100vw)', height: 'calc(60vh + 60vw)'}} src={curvySection} alt="" />
+          <Grid2 container sx={{ margin: 'calc(4vw+5vh)', width: '100vw', height: "90vh", zIndex: 3}}>
+            <Grid2 size={5} sx={{display: 'flex', flexDirection: 'column',alignItems: 'center', justifyContent:'center'}} >
+              <Box sx={{width: 'calc(20vw + 20vh)', marginBottom: 'calc(10vw + 10vh)', marginLeft:'calc(10vw + 10vw)'}}>
+                <Typography variant='h6' color='white'>Satokie Mining</Typography>
+                <Typography variant='h3' color='white' sx={{textWrap: 'wrap'}}>WE BUILD, OWN AND OPERATE DATACENTERS THAT SUPPORT THE BITCOIN NETWORK</Typography>
+              </Box>
+            </Grid2>
+            <Grid2 size={2} sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+                <Box sx={{marginTop: 'calc(10vw + 10vh)', backgroundColor: 'black', width: 'calc(10vw + 10vh)', height: 'calc(10vw + 10vh)', borderRadius: '50%', objectFit: 'contain', overflow: 'hidden', display: 'flex'}}>
+                  <img style={{objectFit: 'cover'}}src={rackImg} alt="" />
+                </Box> 
+            </Grid2>
+            <Grid2 size={3} sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+              <Box sx={{backgroundColor: 'black', width: 'calc(18vw + 18vh)', height: 'calc(18vw + 18vh)', borderRadius: '50%', objectFit: 'contain', overflow: 'hidden', display: 'flex'}}>
+                <img style={{objectFit: 'cover'}} src={siteImg} alt="" />
+              </Box>
+            </Grid2>
+            
+          </Grid2>
+        </Paper>
+        <Paper sx={{width: '100%', height: '100%', borderRadius:'0', backgroundColor: 'blue', position: 'absolute', zIndex: 1}}>
+            
+        </Paper>
       </Paper>
       <Paper id='home-3' sx={{backgroundColor: 'cream', width: '100vw', height: '100vh', borderRadius: '0'}}>
 
